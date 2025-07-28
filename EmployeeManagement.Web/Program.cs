1b11368c413b7f05384d74a79b60744624427714
Lokesh Kumawat <lokesh.kumawat143@gmail.com>
2025-07-29 01:02:51 +0530

    Final check-in

--- EmployeeManagement.Web/Program.cs
+++ EmployeeManagement.Web/Program.cs
@@ -1,20 +1,18 @@
-﻿using EmployeeManagement.Web.Controllers;
-using EmployeeManagement.Web.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Serilog
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.File("Logs/web-log.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
 builder.Host.UseSerilog();
-
 builder.Logging.ClearProviders();
 
+// Configure HTTP Clients
 builder.Services.AddHttpClient("HttpApiClient", client =>
 {
     client.BaseAddress = new Uri("http://localhost:5038/");
@@ -25,30 +23,22 @@ builder.Services.AddHttpClient("HttpsApiClient", client =>
     client.BaseAddress = new Uri("https://localhost:7071/");
 });
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<AppIdentityDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<AppIdentityDbContext>();
-
+// Add Controllers with Views
 builder.Services.AddControllersWithViews()
     .AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.PropertyNamingPolicy = null;
     });
 
+// Build the app
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseStatusCodePagesWithReExecute("/Employee/HandleStatusCode", "?code={0}");
     app.UseExceptionHandler("/Employee/Error");
     app.UseHsts();
-    //app.UseDeveloperExceptionPage();
 }
 else
 {
@@ -59,14 +49,18 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
-app.UseAuthentication();
+
+// No authentication, so no need for UseAuthentication
+// app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Employee}/{action=GetEmployeeList}/{id?}");
 
-app.MapRazorPages();
+// Removed app.MapRazorPages(); since you're not using Razor Pages
 
 app.Run();
